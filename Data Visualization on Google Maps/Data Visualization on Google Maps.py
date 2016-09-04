36d0f961eec4d903fc495a2ee0152c30a8af6502
Xuan Zhou <hinn.chou@gmail.com>
2016-09-04 01:42:54 -0400

    Add files via upload

---
+++ Data Visualization on Google Maps/Data Visualization on Google Maps.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep  4 01:40:58 2016
+
+@author: Hin
+"""
+
+import gmplot
+from pandas import Series, DataFrame
+import pandas as pd
+
+data_sf = pd.read_csv('data_sf.csv')
+
+lats = data_sf['lat']
+
+lons = data_sf['lon']
+
+gmap = gmplot.GoogleMapPlotter(37.770771, -122.413081, 12)
+
+gmap.heatmap(lats, lons, opacity = 0.8)
+
+gmap.draw("mymap.html")
