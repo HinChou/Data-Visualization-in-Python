9f9a94169dfcdfa2b33b83be55bc2e1154cff3b6
Xuan Zhou <hinn.chou@gmail.com>
2016-09-15 15:54:17 -0400

    Update Heatmap of Houses in SF.py

--- Data Visualization on Google Maps/Heatmap of Houses in SF.py
+++ Data Visualization on Google Maps/Heatmap of Houses in SF.py
@@ -12,12 +12,8 @@ import pandas as pd
 
 data_sf = pd.read_csv('data_sf.csv')
 
-lats = data_sf['lat']
-
-lons = data_sf['lon']
-
 gmap = gmplot.GoogleMapPlotter(37.770771, -122.413081, 12)
 
-gmap.heatmap(lats, lons, opacity = 0.8)
+gmap.heatmap(data_sf['lat'], data_sf['lon'], opacity = 0.8)
 
 gmap.draw("mymap.html")
