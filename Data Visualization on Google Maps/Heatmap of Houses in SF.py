bc3feb214944ae38fcb9e8e99ef8513766f99bd2
Xuan Zhou <hinn.chou@gmail.com>
2016-09-21 19:37:20 -0400

    Update Heatmap of Houses in SF.py

--- Data Visualization on Google Maps/Heatmap of Houses in SF.py
+++ Data Visualization on Google Maps/Heatmap of Houses in SF.py
@@ -7,7 +7,6 @@ Created on Sun Sep  4 01:40:58 2016
 
 # Heatmap of Houses in San Francisco(SF)
 import gmplot
-from pandas import Series, DataFrame
 import pandas as pd
 
 data_sf = pd.read_csv('data_sf.csv')
