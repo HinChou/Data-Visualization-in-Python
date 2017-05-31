d91e2945c648297a9a33868431630004bf7ff6c0
Xuan Zhou <hinn.chou@gmail.com>
2017-05-31 23:32:01 +0800

    Update Emoji_Creation.py

--- Emoji_Creation/Emoji_Creation.py
+++ Emoji_Creation/Emoji_Creation.py
@@ -8,13 +8,13 @@ Created on Thu Mar 23 23:05:36 2017
 from PIL import Image, ImageDraw, ImageFont
 
 
-img = Image.open(".\Xie_Emoji_meitu_2.jpg")
-thug_life_text = Image.open(".\Thug-Life-Text_meitu_1.jpg")
-#glasses = Image.open(".\Thug-Life-Glasses-3_meitu_5.png")
-glasses = Image.open(".\Thug-Life-Glasses-3_meitu_3.jpg")
-#hat = Image.open(".\Thug-Life-Hat-1_meitu_1.jpg")
-fire = Image.open(".\Fire_meitu_2.jpg")
-#gun = Image.open(".\Gun.jpg")
+img = Image.open('.\Xie_Emoji_meitu_2.jpg')
+thug_life_text = Image.open('.\Thug-Life-Text_meitu_1.jpg')
+#glasses = Image.open('.\Thug-Life-Glasses-3_meitu_5.png')
+glasses = Image.open('.\Thug-Life-Glasses-3_meitu_3.jpg')
+#hat = Image.open('.\Thug-Life-Hat-1_meitu_1.jpg')
+fire = Image.open('.\Fire_meitu_2.jpg')
+#gun = Image.open('.\Gun.jpg')
 
 # Paste other images on the top of the "img" object
 img.paste(thug_life_text, (150, 0))
